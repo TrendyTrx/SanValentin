23e8ca8a3bb767ba67d1b62202b7d480fd3752ed
TrendyTrx <trendytrx@gmail.com>
2025-02-03 21:43:18 -0500

    Proyecto

---
+++ config.js
@@ -0,0 +1,25 @@
+const CONFIG = {
+    introTitle: 'Feliz San Valentín',
+    introDesc: `Hoy celebramos el amor y la amistad. 
+    Que este día te recuerde lo increíble que eres
+     y cuánto significas para mí. 
+     ❤️`,
+    btnIntro: 'TE AMO',
+    title: '¿Será que me enamoré desde el primer momento? 😳',
+    desc: '¿O fue tu sonrisa la que atrapó mi corazón? 💞',
+    q: '¿Aceptarías salir conmigo este día especial?',
+
+    btnYes: '¡Por supuesto, te adoro! 😍',
+    btnNo: 'Mmm... mejor sigamos como amigos 🤭',
+
+    question: 'De entre más de 7 mil millones de personas... ¿por qué me enamoré de ti? 💘',
+    btnReply: 'Dímelo con amor 💌',
+
+    reply: 'Porque eres lo mejor que me ha pasado 💖',
+
+    mess: '¡Sabía que lo dirías! 🥰 Te quiero muchísimo 😘',
+    messDesc: 'Nos veremos pronto mi niña, escribiremos una hermosa historia desde ahora 💕',
+
+    btnAccept: '¡Sí, sí, sí! 💃✨',
+    messLink: 'https://drive.google.com/uc?export=download&id=15MmTOT7SQPp1QDh9fLuohtuNAKAYkDL8' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
